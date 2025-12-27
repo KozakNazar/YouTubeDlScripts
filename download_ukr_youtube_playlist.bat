3960690a609df0fb3a798defb3269715afe7614b
KozakNazar <55637421+KozakNazar@users.noreply.github.com>
2025-12-27 08:21:35 +0200

    Add files via upload

---
+++ download_ukr_youtube_playlist.bat
@@ -0,0 +1,40 @@
+@echo off
+REM setlocal enabledelayedexpansion REM для !variable!
+set "DOWNLOAD_DIR=%~dp0YtDlpDownloads"
+set "MAX_HEIGHT=720"
+
+REM set "RAW_URL=%~1"
+set "RAW_URL=%*"
+
+if "%RAW_URL%"=="" (
+    echo Please enter YouTube playlist URL:
+    set /p "RAW_URL=URL: "
+)
+
+if "%RAW_URL%"=="" (
+    echo No URL provided. Exiting.
+    pause
+    exit /b 1
+)
+
+if "%RAW_URL:"=%"=="%RAW_URL%" (
+    set "URL="%RAW_URL%""
+) else (
+	set "URL=%RAW_URL%"
+)
+
+echo Downloading...
+echo.
+
+yt-dlp -f "bv*[height<=%MAX_HEIGHT%]+ba[language=uk]/bv*[height<=%MAX_HEIGHT%]+ba/b" ^
+       -o "%DOWNLOAD_DIR%\%%(playlist_title)s\%%(playlist_index)02d - %%(title)s.%%(ext)s" ^
+       --merge-output-format mp4 ^
+       --no-overwrites ^
+       --ignore-no-formats-error ^
+       --continue ^
+       --yes-playlist ^
+       --progress ^
+       %URL%
+
+echo Done!
+pause
---
+++ download_ukr_youtube_video.bat
@@ -0,0 +1,39 @@
+@echo off
+REM setlocal enabledelayedexpansion REM для !variable!
+set "DOWNLOAD_DIR=%~dp0YtDlpDownloads"
+set "MAX_HEIGHT=720"
+
+REM set "RAW_URL=%~1"
+set "RAW_URL=%*"
+
+if "%RAW_URL%"=="" (
+    echo Please enter YouTube video URL:
+    set /p "RAW_URL=URL: "
+)
+
+if "%RAW_URL%"=="" (
+    echo No URL provided. Exiting.
+    pause
+    exit /b 1
+)
+
+if "%RAW_URL:"=%"=="%RAW_URL%" (
+    set "URL="%RAW_URL%""
+) else (
+	set "URL=%RAW_URL%"
+)
+
+echo Downloading...
+echo.
+
+yt-dlp -f "bv*[height<=%MAX_HEIGHT%]+ba[language=uk]/bv*[height<=%MAX_HEIGHT%]+ba/b" ^
+       -o "%DOWNLOAD_DIR%\%%(title)s.%%(ext)s" ^
+       --merge-output-format mp4 ^
+       --no-overwrites ^
+       --ignore-no-formats-error ^
+       --continue ^
+       --progress ^
+       %URL%
+
+echo Done!
+pause
